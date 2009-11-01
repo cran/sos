f46e3ac61318749bcceeaeac49b376947d505f44
Spencer Graves <spencer.graves@prodsyse.com>
2009-11-01 00:00:00 +0000

    version 1.1-7

--- R/grepFn.R
+++ R/grepFn.R
@@ -1,11 +1,11 @@
 grepFn <- function(pattern, x,  column='Function',
-       ignore.case=FALSE, extended=TRUE, perl=FALSE,
+       ignore.case=FALSE, perl=FALSE,
        value=TRUE, fixed=FALSE, useBytes=FALSE, invert=FALSE) {
 ##
 ## 1.  grep
 ##
   g <- grep(pattern, x[, column], ignore.case=ignore.case,
-            extended=extended, perl=perl, value=FALSE, fixed=fixed,
+            perl=perl, value=FALSE, fixed=fixed,
             useBytes=useBytes, invert=invert)
 ##
 ## 2.  value?
